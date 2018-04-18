a2305feb20ab3da806dd830a67a07bd35b2e266b
Jerry <nano@pixelcollider.net>
2018-04-19 01:59:17 +1200

    Raised timer priority to be higher than existing tasks

--- software/lcfr-u/inc/freertos/FreeRTOSConfig.h
+++ software/lcfr-u/inc/freertos/FreeRTOSConfig.h
@@ -91,7 +91,7 @@
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
 #define	configUSE_TIMERS				1
-#define configTIMER_TASK_PRIORITY		3
+#define configTIMER_TASK_PRIORITY		10
 #define configTIMER_QUEUE_LENGTH		10
 #define	configTIMER_TASK_STACK_DEPTH	2048
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
